3be1ebdfedd457630f553ab9f5efdce910262180
Michael Albinson <mikealbinson@gmail.com>
2016-12-03 10:42:27 -0500

    Add gif images

--- runSimulation.py
+++ runSimulation.py
@@ -1,16 +1,21 @@
-import graphics
-import time
+import pygame, time
 
-def createWindow():
-	win = graphics.GraphWin("Sheep Herding Simulation", 1400, 750)
-	win.setBackground("#00FF00")
-	return win
+size = width, height = 320, 240
+speed = [2, 2]
+black = 0, 0, 0
+screen = pygame.display.set_mode(size)
+ball = pygame.image.load("sheep.gif")
+ballrect = ball.get_rect()
 
+while 1:
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT: sys.exit()
+		ballrect = ballrect.move(speed)
+		if ballrect.left < 0 or ballrect.right > width:
+			speed[0] = -speed[0]
+		if ballrect.top < 0 or ballrect.bottom > height:
+			speed[1] = -speed[1]
 
-win = createWindow()
-time.sleep(2000)
-
-
-
-
-
+		screen.fill(black)
+		screen.blit(ball, ballrect)
+		pygame.display.flip()
