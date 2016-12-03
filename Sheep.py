da89530c9b6e5718e2ca7180e2b563a809fe431e
vincihb <user@Users-MacBook-Pro.local>
2016-12-03 14:00:13 -0500

    Filled out sheep class

--- Sheep.py
+++ Sheep.py
@@ -1,5 +1,10 @@
 from pygame import image
+<<<<<<< Updated upstream
 from util import randomPosition
+=======
+import math import pow
+
+>>>>>>> Stashed changes
 
 sheep = image.load("small-sheep.gif")
 leaderSheep = image.load("leaderSheep.gif")
@@ -11,21 +16,31 @@ class Sheep():
 		self.position = randomPosition()
 		self.detectionRadius = 1
 		self.speed = 3;
+        self.neighbor[0, 0]
+        self.velocity[0,0]
 		if (isLeader):
 			self.image = leaderSheep.convert()
 		else:
 			self.image = sheep.convert()
-		self.imRect = self.image.imageRect()
+        self.imRect = self.image.imageRect()
+
 
 	def detectDrone(self):
-		pass
+        if (pow((pow(Drone.position[x]-self.position[x], 2))+pow(Drone.position[y]-self.position, 0.5))) < self.detectionRadius:
+            temp_vector[-(Drone.position[x]-self.position[x]), -(Drone.position[y] - self.position[y])]
+        return temp_vector
 
 	def detectWolf(self):
-		pass
+		if (pow((pow(Wolf.position[x]-self.position[x], 2))+pow(Wolf.position[y]-self.position, 0.5))) < self.detectionRadius:
+            temp_vector[-(Wolf.position[x]-self.position[x]), -(Wolf.position[y] - self.position[y])]
+        return temp_vector
 
-	def followNeighbor(self, neighborPos):
-		pass
 
-	def move(self):
+	def followNeighbor(self):
+        findNeighbor(self)
+
+
+	def move(self, temp_vector):
+        self.velocity[temp_vector[0]*self.speed, temp_vector[]
 		pass
 
