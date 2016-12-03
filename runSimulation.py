770d85478e334bd750f973f60f93d379a79c7b49
Michael Albinson <mikealbinson@gmail.com>
2016-12-03 09:34:58 -0500

    getting started

---
+++ runSimulation.py
@@ -0,0 +1,16 @@
+import graphics
+import time
+
+def createWindow():
+	win = graphics.GraphWin("Sheep Herding Simulation", 1400, 750)
+	win.setBackground("#00FF00")
+	return win
+
+
+win = createWindow()
+time.sleep(2000)
+
+
+
+
+
